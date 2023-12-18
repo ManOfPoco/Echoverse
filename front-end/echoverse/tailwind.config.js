7ef329ae3661234864a5244cc8c0247ecfcf596f
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-12-18 18:58:26 +0200

    style: add base tailwind theme styles such as breakpoints, colors, fonts

--- front-end/echoverse/tailwind.config.js
+++ front-end/echoverse/tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '576px',
+      'md': '960px',
+      'lg': '1440px',
+    },
+    colors: {
+      'black': {
+        'light': '#0E1C26',
+        'dark': '#000000',
+      },
+      'gray': {
+        'light': '#999999',
+        'charcoal': '#374151',
+        'dark': '#262A2F',
+      },
+      'silver': '#557C93',
+      'purple': '#645BF0',
+      'orange': '#E3792D',
+      'blue': '#4A92FE',
+      'red': '#CE3124',
+      'turquoise': '#6EE2F5',
+    },
+    fontFamily: {
+      'archivo-black': ['Archivo Black', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+      'monoton': ['Monoton', 'sans-serif'],
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 }
