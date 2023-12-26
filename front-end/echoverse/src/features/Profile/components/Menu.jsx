d391b0dca9f514e2d236e88d8761136728e73cc4
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-12-26 18:08:40 +0200

    fix: fix a bug preventing users from switching between games and saved pages in the profile

--- front-end/echoverse/src/features/Profile/components/Menu.jsx
+++ front-end/echoverse/src/features/Profile/components/Menu.jsx
@@ -6,29 +6,31 @@ function Menu({ action, username }) {
 
     return (
         <>
-            <div className="flex justify-center gap-14">
-                <h2
-                    className={`cursor-pointer border-blue-light px-2 pb-2 pt-4 text-xl ${
-                        action === "games" && "border-b-4"
-                    }`}
-                    onClick={() => navigate(`/${username}`)}
-                >
-                    Games
-                </h2>
-                <h2
-                    className={`cursor-pointer border-blue-light px-2 pb-2 pt-4 text-xl ${
-                        action === "saved" && "border-b-4"
-                    }`}
-                    onClick={() => navigate(`/${username}/saved`)}
-                >
-                    Saved
-                </h2>
+            <div className="flex justify-center">
+                <div className="flex gap-14 z-10">
+                    <h2
+                        className={`cursor-pointer border-blue-light px-2 pb-2 pt-4 text-xl ${
+                            action === "games" && "border-b-4"
+                        }`}
+                        onClick={() => navigate(`/${username}`)}
+                    >
+                        Games
+                    </h2>
+                    <h2
+                        className={`cursor-pointer border-blue-light px-2 pb-2 pt-4 text-xl ${
+                            action === "saved" && "border-b-4"
+                        }`}
+                        onClick={() => navigate(`/${username}/saved`)}
+                    >
+                        Saved
+                    </h2>
+                </div>
 
-                <div className="absolute hidden h-14 w-full max-w-[1080px] flex-col items-end justify-center px-5 md:flex lg:px-0">
+                <div className="absolute hidden h-14 w-full max-w-[1080px] items-center justify-end px-5 md:flex lg:px-0">
                     <SearchForm />
                 </div>
             </div>
-            <div className="flex flex-col pt-5 items-center md:hidden">
+            <div className="flex flex-col items-center pt-5 md:hidden">
                 <SearchForm />
             </div>
         </>
