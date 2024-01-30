3dfa66693631654de30dbebe4fd6cca30899d042
Kyryl Kliuchnikov <kliuchnikovkyryl@gmail.com>
2024-01-30 19:13:32 +0200

    style: fix style of blue background if user already reacted to a message with a specific reaction

--- front-end/echoverse/src/features/Messages/components/MessageReaction.jsx
+++ front-end/echoverse/src/features/Messages/components/MessageReaction.jsx
@@ -2,10 +2,10 @@ function MessageReaction({ reaction }) {
     const { reaction: reactionEmoji, quantity, isReacted } = reaction;
     return (
         <div
-            className={`flex cursor-pointer items-center gap-1.5 rounded-md bg-gray-dark/90 px-1.5 py-1 font-medium  hover:text-white ${
+            className={`flex cursor-pointer items-center gap-1.5 rounded-md px-1.5 py-1 font-medium  hover:text-white ${
                 isReacted
                     ? "bg-blue-light/15 text-white outline outline-1 outline-blue-light"
-                    : "text-white/75 outline-gray-light/40 hover:outline hover:outline-1"
+                    : "bg-gray-dark/90 text-white/75 outline-gray-light/40 hover:outline hover:outline-1"
             }`}
         >
             <span className="text-base leading-4">{reactionEmoji}</span>
